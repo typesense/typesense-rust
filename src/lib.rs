da6af0d5a4eb54764d8f213717b4f05dff420f4a
Luis Moreno <morenol@users.noreply.github.com>
2021-04-22 21:39:22 -0400

    doc: Add documentation introduction for the library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+#![deny(missing_docs)]
+//! # Typesense
+//!
+//! Welcome to typesense, the rust library for the Typesense API.
