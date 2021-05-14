77cb1cbbaf4979b38ce7f4aad521206cd23b885c
Emmanuel Antony <emmanuelantony2000@gmail.com>
2021-05-15 02:30:01 +0530

    Transport Implementation (#8)
    * Initial implementation
    
    * Fixed issues, introduced tests and features
    
    * ci: run tests in other platforms (windows, wasm)
    
    * Initial implementation
    
    * Fixed issues, introduced tests and features
    
    * Cleaned up the code, testable code
    
    * Fixed ci
    
    * Fixed ci
    
    * Removed .unwrap() where not necessary
    
    * Added comments and edited ci
    
    Co-authored-by: Luis Moreno <morenol@users.noreply.github.com>
    Co-authored-by: Luis Moreno <13-10934@usb.ve>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #![deny(missing_docs)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 //! # Typesense
 //!
 //! Welcome to typesense, the rust library for the Typesense API.
 
 mod error;
+pub mod transport;
 
 pub use error::{Result, TypesenseError};
