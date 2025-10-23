f56ca8026e13f6dcc74fbf686de1b16a4f094b38
Hayden Hung Hoang <94696449+haydenhoang@users.noreply.github.com>
2025-10-23 19:21:16 +0700

    Dev command to run test & clean up (#56)
    * feat: xtask to run test & clean up
    
    * feat(xtask): aliased command to run wasm test
    
    * typo
    
    * readme: wasm test clarity
    
    * xtask: test clean up only delete collection with `test_` prefix

---
+++ xtask/src/test_clean.rs
@@ -0,0 +1,68 @@
+use std::time::Duration;
+use typesense::{Client, ExponentialBackoff, models::GetCollectionsParameters};
+
+async fn clean_test_artifacts() {
+    let client = Client::builder()
+        .nodes(vec!["http://localhost:8108"])
+        .api_key("xyz")
+        .healthcheck_interval(Duration::from_secs(5))
+        .retry_policy(ExponentialBackoff::builder().build_with_max_retries(3))
+        .connection_timeout(Duration::from_secs(3))
+        .build()
+        .expect("Failed to create Typesense client");
+
+    let collections = client
+        .collections()
+        .retrieve(GetCollectionsParameters::new())
+        .await
+        .expect("Get all collections failed!");
+
+    println!("Cleaning up test collections...");
+
+    let mut collection_count = 0;
+
+    for collection in collections.iter() {
+        if !collection.name.starts_with("test_") {
+            continue;
+        }
+
+        if let Err(err) = client
+            .collection_schemaless(&collection.name)
+            .delete()
+            .await
+        {
+            eprintln!("Failed to delete {}: {}", collection.name, err);
+        } else {
+            collection_count += 1;
+            println!("Deleted {}", collection.name);
+        }
+    }
+    println!("Deleted {} test collections.", collection_count);
+    println!("✅ Cleanup complete.");
+}
+
+pub async fn test_clean(is_wasm: bool, args: Vec<String>) {
+    let status = if is_wasm {
+        println!("Running wasm-pack test...");
+        std::process::Command::new("wasm-pack")
+            .arg("test")
+            .arg("--headless")
+            .arg("--chrome")
+            .args(&args)
+            .arg("typesense")
+            .status()
+            .expect("Failed to run wasm-pack test")
+    } else {
+        println!("Running cargo test with arguments: {}", args.join(" "));
+        std::process::Command::new("cargo")
+            .arg("test")
+            .args(&args)
+            .status()
+            .expect("Failed to run cargo test")
+    };
+
+    clean_test_artifacts().await;
+
+    // Propagate cargo test exit code
+    std::process::exit(status.code().unwrap_or(1));
+}
